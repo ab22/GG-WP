00c8ebffb14a6e5fce3c276ce1ca7b9fb41855a0
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2015-03-15 11:25:03 -0600

    Added all regions

--- services/region.py
+++ services/region.py
@@ -1,5 +1,15 @@
 class Region():
-    supported_regions = ('na', 'lan')
+    supported_regions = (
+        'na',
+        'lan',
+        'las',
+        'euw',
+        'eune',
+        'kr',
+        'oce',
+        'ru',
+        'tr'
+    )
 
     @staticmethod
     def is_region_valid(region):
@@ -16,4 +26,20 @@ class Region():
             return 'NA1'
         elif region == 'lan':
             return 'LA1'
+        elif region == 'br':
+            return 'BR1'
+        elif region == 'las':
+            return 'LA2'
+        elif region == 'oce':
+            return 'OC1'
+        elif region == 'eune':
+            return 'EUN1'
+        elif region == 'tr':
+            return 'TR1'
+        elif region == 'ru':
+            return 'RU'
+        elif region == 'euw':
+            return 'EUW1'
+        elif region == 'kr':
+            return 'KR'
         return ''
