787e286d171dd3ae5a778d2c9917c5e6645ed455
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2015-03-15 11:07:33 -0600

    Moving logic to services

--- index.py
+++ index.py
@@ -1,8 +1,6 @@
 import config
 import logging
-import motor
 import services
-import tornadoredis
 import tornado
 
 
@@ -11,44 +9,21 @@ def configure_logger(**kwargs):
     return logging.getLogger()
 
 
-def configure_mongodb(connection):
-    default_port = 27017
-    host = connection.get('host', None)
-    port = connection.get('port', default_port)
-    db = connection['database']
-    client = motor.MotorClient(host, port)
-    return client, client[db]
-
-
-def configure_redis(connection):
-    default_port = 6379
-    host = connection.get('host', None)
-    port = connection.get('port', default_port)
-    pw = connection.get('password', None)
-    redis = tornadoredis.Client(host, port, password=pw)
-    redis.connect()
-    return redis
-
-
 def main():
     # Get config parameters
     settings = config.settings
 
-    databases = settings.DATABASES
     app_port = settings.APP_PORT
     logger_settings = settings.LOGGER_SETTINGS
 
     log = configure_logger(**logger_settings)
-    client, db = configure_mongodb(databases['mongodb'])
-    cachedb = configure_redis(databases['redis'])
-    services.initialize(db, cachedb)
+    services.initialize()
 
     params = {
         'handlers': config.ROUTES,
         'debug': settings.DEBUG,
         'template_path': settings.VIEWS_PATH,
         'static_path': settings.STATIC_PATH,
-        'db': db,
         'log': log,
         'cookie_secret': settings.SECRET_KEY,
         'login_url': settings.LOGIN_URL
--- services/__init__.py
+++ services/__init__.py
@@ -1,3 +1,7 @@
+import config
+import motor
+import tornadoredis
+
 from services.region import Region
 from services.summoner import Summoner
 from services.match import Match
@@ -8,9 +12,30 @@ from services.summoner_spell import SummonerSpell
 db = None
 cachedb = None
 
+DEFAULT_MONGODB_PORT = 27017
+DEFAULT_REDIS_PORT = 6379
 
-def initialize(database, cache_database):
+def configure_mongodb(connection):
+    host = connection.get('host', None)
+    port = connection.get('port', DEFAULT_MONGODB_PORT)
+    db = connection['database']
+    client = motor.MotorClient(host, port)
+    return client, client[db]
+
+
+def configure_redis(connection):
+    host = connection.get('host', None)
+    port = connection.get('port', DEFAULT_REDIS_PORT)
+    pw = connection.get('password', None)
+    redis = tornadoredis.Client(host, port, password=pw)
+    redis.connect()
+    return redis
+
+
+def initialize():
     global db
     global cachedb
-    db = database
-    cachedb = cache_database
+    settings = config.settings
+    databases = settings.DATABASES
+    client, db = configure_mongodb(databases['mongodb'])
+    cachedb = configure_redis(databases['redis'])
