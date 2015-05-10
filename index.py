4fd0438e32f7df285ab13c7c9869c6f6c46895d5
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2015-05-10 14:51:47 -0600

    Index code cleanup

--- index.py
+++ index.py
@@ -17,17 +17,16 @@ def main():
     log = configure_logger(**logger_settings)
     services.initialize()
 
-    params = {
-        'handlers': config.ROUTES,
-        'debug': settings.DEBUG,
-        'template_path': settings.VIEWS_PATH,
-        'static_path': settings.STATIC_PATH,
-        'log': log,
-        'cookie_secret': settings.SECRET_KEY,
-        'login_url': settings.LOGIN_URL,
-        'db': services.db
-    }
-    application = tornado.web.Application(**params)
+    application = tornado.web.Application(
+        handlers=config.ROUTES,
+        debug=settings.DEBUG,
+        template_path=settings.VIEWS_PATH,
+        static_path=settings.STATIC_PATH,
+        log=log,
+        cookie_secret=settings.SECRET_KEY,
+        login_url=settings.LOGIN_URL,
+        db=services.db
+    )
 
     log.info('Starting server...')
     application.listen(app_port)
